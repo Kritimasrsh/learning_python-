87a4de609add00b3925e0bb71886f13519bacb7a
Kritima <Kritima.Shrestha_22@eemc.edu.np>
2026-04-22 17:12:08 +0545

    Name

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 for i in range(10) :
     print("Hey there, welcome to Python programming learning Journey!")
+
+name = input("What is your name? ")
+print("Hello " + name + ", welcome to Python programming learning Journey!")
+
+name = "Kritima"
+print("Hello " + name)
