b3b965e0126ef4dfc884689afabe4a9f603af8a8
Kritima <Kritima.Shrestha_22@eemc.edu.np>
2026-04-26 23:08:21 +0545

    Comparing

---
+++ compare.py
@@ -0,0 +1,15 @@
+print (10>1)
+print (10<1)
+
+print("cat" == "dog")
+
+print(1 != 2) #!= not equal to operator
+print(1 == 1 and 2 == 2) #and operator
+
+print(1 == "1")
+
+print("Yellow" > "Cyan" and "Brown" > "Magenta")
+
+print(25 < 50 or 1 != 2)
+
+print(not 42 == "Answer")
