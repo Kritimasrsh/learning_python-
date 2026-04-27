8f9adcf91efebac15e9cf96479c7a2bfa7ca695a
Kritima <Kritima.Shrestha_22@eemc.edu.np>
2026-04-27 14:44:32 +0545

    Comparing

--- compare.py
+++ compare.py
@@ -4,7 +4,7 @@ print (10<1)
 print("cat" == "dog")
 
 print(1 != 2) #!= not equal to operator
-print(1 == 1 and 2 == 2) #and operator
+print(1 == 1 and 2 == 2)
 
 print(1 == "1")
 
@@ -13,3 +13,6 @@ print("Yellow" > "Cyan" and "Brown" > "Magenta")
 print(25 < 50 or 1 != 2)
 
 print(not 42 == "Answer") #if the expression is true it returns false likewise.
+
+print("A dog" < "A mouse") #checks strings in alphabetical order 
+print(9999+8888 > 100*100)
