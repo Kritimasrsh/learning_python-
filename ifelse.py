3bc702e045c64036a7a3ed34c48c9480e69e65f6
Kritima <Kritima.Shrestha_22@eemc.edu.np>
2026-04-27 14:42:51 +0545

    if-else-elif statement

--- ifelse.py
+++ ifelse.py
@@ -25,3 +25,16 @@ def hint_username(username):
     else:
         print("Username is valid.")
 
+def calculate_storage(filesize):
+    block_size = 4096
+    full_blocks = filesize // block_size
+    partial_block_remainder = filesize % block_size
+    
+    if partial_block_remainder > 0:
+        return (full_blocks + 1) * block_size
+    return full_blocks * block_size
+
+print(calculate_storage(1))    
+print(calculate_storage(4096))
+print(calculate_storage(4097)) 
+print(calculate_storage(6000)) 
